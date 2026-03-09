d32a392da3f8e9cc1e5042d5e91f636613337a80
vamshi Amudala <vamshipatel126@gmail.com>
2026-03-10 00:18:48 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import Ideas from "./pages/Ideas";
 
