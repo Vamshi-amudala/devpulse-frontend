9af774e939911eaea9f14212234e3403b0f925bb
vamshi Amudala <vamshipatel126@gmail.com>
2026-03-10 00:03:03 +0530

    cors origin fixed

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, ''),
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+  }
 })
